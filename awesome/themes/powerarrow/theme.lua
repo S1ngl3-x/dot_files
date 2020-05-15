a6b3efe9c23c17f2e35b578c1d633287aa30dfdd
lipowada <lipowada@fel.cvut.cz>
2020-05-15 14:52:21 +0200

    :lipstick: Make date and time more readable

--- awesome/themes/powerarrow/theme.lua
+++ awesome/themes/powerarrow/theme.lua
@@ -108,7 +108,8 @@ local separators = lain.util.separators
 -- Textclock
 os.setlocale(os.getenv("LANG")) -- to localize the clock
 local clockicon = wibox.widget.imagebox(theme.widget_clock)
-local mytextclock = wibox.widget.textclock(markup("#7788af", "%A %d %B ") .. markup("#ab7367", ">") .. markup("#de5e1e", " %H:%M "))
+--local mytextclock = wibox.widget.textclock(markup("#7788af", "%A %d %B ") .. markup("#ab7367", ">") .. markup("#de5e1e", " %H:%M "))
+local mytextclock = wibox.widget.textclock(markup("#C0C0C0", "%A %d %B ") .. markup("#de5e1e", ">") .. markup("#FFFFFF", " %H:%M "))
 mytextclock.font = theme.font
 
 -- Calendar
