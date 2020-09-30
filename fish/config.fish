90fac7e54f2c29176e2ca9b796bfe525797ac68b
lipowada <lipowada@fel.cvut.cz>
2020-09-30 21:16:43 +0200

    💄 Try to style terminal

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,18 @@ alias v=nvim
 alias xc="xclip -selection clipboard"
 set fish_greeting
 set -gx EDITOR nvim
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_showupstream informative
+set -g __fish_git_prompt_showdirtystate yes
+set -g __fish_git_prompt_char_stateseparator ' '
+set -g __fish_git_prompt_char_cleanstate '✔'
+set -g __fish_git_prompt_char_dirtystate '✚'
+set -g __fish_git_prompt_char_invalidstate '✖'
+set -g __fish_git_prompt_char_stagedstate '●'
+set -g __fish_git_prompt_char_stashstate '⚑'
+set -g __fish_git_prompt_char_untrackedfiles '?'
+set -g __fish_git_prompt_char_upstream_ahead ''
+set -g __fish_git_prompt_char_upstream_behind ''
+set -g __fish_git_prompt_char_upstream_diverged 'ﱟ'
+set -g __fish_git_prompt_char_upstream_equal ''
+set -g __fish_git_prompt_char_upstream_prefix ''''
