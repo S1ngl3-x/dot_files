6e0b2ff181b75dfad583af73ecc6759981ddc3f2
adam.lipowski <adam.lipowski@fidoo.com>
2024-02-06 21:33:20 +0100

    :memo: Document why powerline prompt is automatically loaded

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,3 +1,4 @@
+# overrides the default prompt, fish_prompt is a special fish function name
 function fish_prompt
     powerline-go -error $status -modules "user,cwd" -modules-right "git" -git-mode "simple" -cwd-max-depth 3
 end
