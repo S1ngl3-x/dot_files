0a443dfa1a6e452827442d201ed73ef087efe7a2
S1ngl3 <adam.lipowski@email.cz>
2024-09-18 11:56:26 +0200

    fix- linux powerline shell prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,13 +1,8 @@
-# overrides the default prompt, fish_prompt is a special fish function name
-#function fish_prompt
-#    powerline-go -error $status -modules "user,cwd" -modules-right "git" -git-mode "simple" -cwd-max-depth 3
-#end
-
 function fish_prompt
     switch $OS_TYPE
         case macos
             echo -n (whoami)(prompt_pwd) '> '
         case fedora
-            codium --ozone-platform-hint=auto
+            powerline-go -error $status -modules "user,cwd" -modules-right "git" -git-mode "simple" -cwd-max-depth 3
     end
 end
